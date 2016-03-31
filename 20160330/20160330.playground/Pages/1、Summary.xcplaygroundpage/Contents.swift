e41f2b1c3efc499d7a04f4ac4f5bb0114220656f
shanksyang <shanks@weedo.hk>
2016-03-31 08:30:15 +0800

    20160330q&a

---
+++ 20160330/20160330.playground/Pages/1、Summary.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+/*:
+# 每周 Swift 问答：关于 for-loop 的问题
+### **Version:** 第十五期
+### **Date:** 2016-03-30
+### **Author:** [shanks](http://codebuild.me)
+### 更多Swift优秀文章请访问 [SwiftGG](http://swift.gg)
+
+![](http://static.zybuluo.com/numbbbbb/wox80orj8gmlh0zumt3a0x23/logo.png)
+
+*/
+
+
+/*:
+
+#### 发现每周整理的问题比较零散，并且每个问题浅尝辄止，没有深入去讨论和分析。这周开始，尝试对一个问题进行深入讨论，加深每个知识点的影响。后面有余力把 stackoverflow 上的类似问题都归档一下，慢慢形成一些知识整理。这种形式感觉会好一些。
+    今天我们就谈谈关于 for 循环的问题。
+ 
+ 
+
+
+### **本周整理问题如下:**
+Question1:
+	[For loop decrement in Swift 2.2](Question1)
+
+
+
+
+
+*/
+
+
+
+//: [下一个问题](@next)
+
+
+
+
+
+
