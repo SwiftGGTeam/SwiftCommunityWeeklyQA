771bf60da5dabb101e72d63a2f735d1e4180c220
shanksyang <shanks@weedo.hk>
2016-04-13 23:08:47 +0800

    20160413

---
+++ 20160413/available.playground/Pages/available.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//
+//if  {
+//    print("ios9")
+//} else {
+//    print("another")
+//}
+
+//#available(iOS 7.0, OSX 10.12, *)
+
+@available(iOS, introduced=9.0)
+func myMethod() {
+    // do something
+}
